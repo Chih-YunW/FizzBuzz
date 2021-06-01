eb63e5cc07bf4bb96d047d62b63336b0f9babb58
Chih Yun <wuchihy@oregonstate.edu>
2021-05-31 17:18:00 -0700

    wrote second test: printing Buzz if a number is multiple of 5 and not 3

--- test_fb.py
+++ test_fb.py
@@ -6,4 +6,7 @@ def test_print_FizzBuzz():
 def test_print_Fizz():
 	value = (3)
 	assert fizzBuzz.fizzBuzz(value) == "Fizz"
+def test_print_Buzz():
+        value = (5)
+        assert fizzBuzz.fizzBuzz(value) == "Buzz"
 
